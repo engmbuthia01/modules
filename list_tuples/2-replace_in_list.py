5e406ae2845f849d7c974d4b10513b229dd48aae
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-02-24 00:18:56 -0800

    a function that replaces an element of a list at a specific position

---
+++ list_tuples/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""A module containing a function that replaces
+an element of a list  at a specific position
+"""
+
+def replace_in_list(my_list, idx, element):
+    if < 0 and idx > len(my_list):
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
