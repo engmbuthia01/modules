b085c49aa7a24a09f97f3773e8e81372c94599d6
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-03-24 11:21:11 -0700

    public instance method

---
+++ classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""This is a module that
+contains a class square
+that defines a square
+"""
+
+class Square:
+    """defines a square by size"""
+    def __init__(self, size=0):
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return size ** 2
