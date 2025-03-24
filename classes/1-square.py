a5b22729adc0d17c2f15d8259781786f78133994
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-03-24 05:35:18 -0700

    defines a square by size

---
+++ classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This is a module that
+contains a class square
+that defines a square
+"""
+
+class Square:
+    """defines a square by size"""
+    def __init__(self, size):
+        self.__size = size
