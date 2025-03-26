283c3c7991249d252e025c41643c24bd9fcfd771
engmbuthia01 <kevinmbuthia3354@gmail.com>
2025-03-26 15:02:43 -0700

    private instance attribute

--- more_classes/2-rectangle.py
+++ more_classes/2-rectangle.py
@@ -40,7 +40,7 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
-        if self.width == 0 or self.height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         else:
-            return 2 * (self.width + self.height)
+            return 2 * (self.__width + self.__height)
